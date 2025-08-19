ccb28400f9999d32faeffb353dacf1ca1b6d217f
Santos Romero <santosromeroreyna@gmail.com>
2025-08-19 16:34:48 -0300

    feat: agregue función calcularPromedio (CON error - división por 0)

--- src/app.js
+++ src/app.js
@@ -4,3 +4,10 @@ function saludar() {
 }
 
 saludar();
+
+// ERROR SIMULADO: División por cero
+function calcularPromedio(numeros) {
+    return numeros.reduce((a, b) => a + b) / 0; // BUG: división por 0
+}
+
+console.log(calcularPromedio([1, 2, 3])); // Esto dará Infinity
